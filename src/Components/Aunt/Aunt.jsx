071e0e24a929c7851fb8d07c8b40184ad9ea2ed3
hasanalam24 <hasanalam365@gmail.com>
2024-03-22 15:49:40 +0600

    use context api and use multiple components keep data add btn click mojadar

--- src/Components/Aunt/Aunt.jsx
+++ src/Components/Aunt/Aunt.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Cousin from '../Cousin/Cousin';
+import { MoneyContext } from '../GrandPa/GrandPa';
 
 const Aunt = () => {
+    const [money, setMoney] = useContext(MoneyContext)
     return (
         <div>
             <h1>Aunt</h1>
@@ -10,6 +12,9 @@ const Aunt = () => {
                 <Cousin name={'Rubaiya'}></Cousin>
                 {/* name hocce prop hisabe jacche */}
             </section>
+
+            <p>Money:{money}</p>
+            <button onClick={() => setMoney(money + 1000)}>Add 1000 tk</button>
         </div>
     );
 };
--- src/Components/GrandPa/GrandPa.jsx
+++ src/Components/GrandPa/GrandPa.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunt from "../Aunt/Aunt";
 import Daad from "../Daad/Daad";
 import Uncle from "../Uncle/Uncle";
@@ -7,19 +7,28 @@ import './GrandPa.css'
 
 export const AssetContext = createContext('gold')
 
+export const MoneyContext = createContext(1000)
+
 const GrandPa = () => {
     const assetNimu = 'Diamond'
+
+    const [money, setMoney] = useState(1000)
+
     return (
         <div className="grandPa">
             <h1>GrandDabba</h1>
 
-            <AssetContext.Provider value="gold">
-                <section className=" flex">
-                    <Daad></Daad>
-                    <Uncle assetNimu={assetNimu}></Uncle>
-                    <Aunt></Aunt>
-                </section>
-            </AssetContext.Provider>
+            <p>Net Money: {money}</p>
+
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
+                    <section className=" flex">
+                        <Daad></Daad>
+                        <Uncle assetNimu={assetNimu}></Uncle>
+                        <Aunt></Aunt>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
 
         </div>
     );
